ffd0216cdfcaba29939de2d40cb9a9a6d7cd176e
Jaynee De Claro <jeeleneejaysondeclaro@gmail.com>
2017-11-07 23:37:10 +0800

    amin folder and files: css and php/html

---
+++ admin/admin_history.php
@@ -0,0 +1,164 @@
+<?php include 'header.html';?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Del Leonor Hotel | Admin</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap.min.css" type="text/css" rel="stylesheet">
+
+</head>
+	<br><br><br>
+
+
+  <div class="pool">
+    <div class="table" id="total_user" style="background-color: white; width: 90%; margin-left: 1%;">
+      <table class="table" class="table table-hover">
+      <table class="table table-bordered" style="border: 1px solid: #ddd;">
+      <thead>
+      <tr>
+      <th><center>INVOICE NO</center></th>
+      <th colspan="6"><center>AMENITIES</center></th>
+
+      <th><center>TOTAL NO. OF TICKETS</center></th>
+      <th><center>AMOUNT TO PAY</center></th>
+      <th><center>USERNAME</center></th>
+    </tr>
+    <tr>
+      <th></th>
+      <th colspan="3"><center>WAVEPOOL</center></th>
+      <th colspan="3"><center>GIANT POOL</center></th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+    <tr>
+      <th></th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+
+    <tr>
+      <th scope="row">01</th>
+      <td><center>2</center></td>
+      <td><center>5</center></td>
+      <td><center>0</center></td>
+      <td><center>2</center></td>
+      <td><center>5</center></td>
+      <td><center>0</center></td>
+      <td><center>14</center></td>
+      <td><center>3200</center></td>
+      <td>Angelica</td>
+    </tr>
+
+    <tr>
+      <th scope="row">02</th>
+      <td><center>0</center></td>
+      <td><center>2</center></td>
+      <td><center>0</center></td>
+      <td><center>0</center></td>
+      <td><center>2</center></td>
+      <td><center>0</center></td>
+      <td><center>4</center></td>
+      <td><center>800</center></td>
+      <td>Jeelenee</td>
+    </tr>
+
+    <tr>
+      <th scope="row">02</th>
+      <td><center>0</center></td>
+      <td><center>5</center></td>
+      <td><center>5</center></td>
+      <td><center>0</center></td>
+      <td><center>5</center></td>
+      <td><center>5</center></td>
+      <td><center>10</center></td>
+      <td><center>3500</center></td>
+      <td>Kyle</td>
+    </tr>
+
+  </thead>
+  </div>
+  
+  <div class="rides">
+    <div class="table" id="total_user" style="background-color: white;">
+<table class="table" class="table table-hover">
+<table class="table table-bordered" style="border: 1px solid: #ddd;">
+    <thead>
+    <tr>
+      <br><br><br><br>
+      <th><center>INVOICE NO</center></th>
+      <th colspan="24"><center>RIDES</center></th>
+      <th><center>NO. OF TICKETS</center></th>
+      <th><center>AMOUNT TO PAY</center></th>
+      <th><center>USERNAME</center></th>
+    </tr>
+    <tr>
+      <th></th>
+      <th colspan="3"><center>ZIPLINE</center></th>
+      <th colspan="3"><center>C.CAR</center></th>
+      <th colspan="3"><center>B.CAR</center></th>
+      <th colspan="3"><center>ATV</center></th>
+      <th colspan="3"><center>B.CART</center></th>
+      <th colspan="3"><center>G.KART</center></th>
+      <th colspan="3"><center>SEGWAY</center></th>
+      <th colspan="3"><center>B.BOAT</center></th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+    <tr>
+      <th></th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th>K</th>
+      <th>A</th>
+      <th>D</th>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+
+    <tr>
+      <th scope="row">01</th>
+      <td>2</td>
+      <td>5</td>
+      <td>0</td>
+    </tr>
+
+</div>
+</table>
+</table>
+</div>
+</div>
+</table>
+</div>
+</html>
---
+++ admin/revenue.php
@@ -0,0 +1,48 @@
+<?php include 'header.html';?>
+<?php
+//index.php
+$connect = mysqli_connect("localhost", "root", "", "pos");
+$query = "SELECT * FROM revenue";
+$result = mysqli_query($connect, $query);
+$chart_data = '';
+while($row = mysqli_fetch_array($result))
+{
+ $chart_data .= "{ day:'".$row["day"]."', profit:".$row["profit"].", purchase:".$row["purchase"].", sale:".$row["sale"]."}, ";
+}
+$chart_data = substr($chart_data, 0, -2);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Del Leonor Hotel | Admin</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="morris.css">
+  	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  	<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+  	<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
+</head>
+<body>
+	<br><br>
+	<div class="container" style="width:900px;">
+   <h2 align="center">D' Leonor`s Daily Revenue</h2>   
+   <br /><br />
+   <div id="chart"></div>
+  </div>
+  
+</body>
+</html>
+<script>
+	Morris.Line({
+	 element : 'chart',
+	 data:[<?php echo $chart_data; ?>],
+	 xkey:'day',
+	 ykeys:['profit', 'purchase', 'sale'],
+	 labels:['Profit', 'Purchase', 'Sale'],
+	 hideHover:'auto',
+	 stacked:true
+});
+</script>
