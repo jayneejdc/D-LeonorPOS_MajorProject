3e0ba43ad082fd4016da2c54bd968174de7147f2
Kylie Gwapo <32198931+greataceofspades@users.noreply.github.com>
2017-11-08 00:00:04 +0800

    Add files via upload
    offline links

--- admin/revenue.php
+++ admin/revenue.php
@@ -20,7 +20,7 @@ $chart_data = substr($chart_data, 0, -2);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="morris.css">
-  	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  	<script src="js/jquery.min.js"></script>
   	<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
   	<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 
