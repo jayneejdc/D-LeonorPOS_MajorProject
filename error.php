95f87b755e15a143c1969a0e44e4e9291bce6d98
Jaynee De Claro <32161131+jayneejdc@users.noreply.github.com>
2017-12-07 18:18:02 +0800

    error

--- error.php
+++ error.php
@@ -3,4 +3,17 @@ $message="";
 session_start();
 $message = $_SESSION['message'];
 ?>
-<span><?php echo $message; ?></span>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error 404</title>
+	<link rel="stylesheet" type="text/css" href="css/welcome.css">
+</head>
+<body>
+	<div class="hue" style="background-color: rgba(255, 255, 255, 0.8); width: 50%; height: 20%; margin-top: 15%; text-align: center; margin-left: 20%; padding: 2%;">
+		<br><br>
+		<span><?php echo $message; ?></span>
+		 <button style="float: left; margin-left: 30%; margin-top: 5%; width: 30%;" class="button"> Back to Main page</button>
+	</div>
+</body>
+</html>
