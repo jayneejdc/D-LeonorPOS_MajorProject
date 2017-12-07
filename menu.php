3f30ae878b1461cf76e689f00ff523fc749e52c0
Kylie Gwapo <32198931+greataceofspades@users.noreply.github.com>
2017-12-07 17:42:15 +0800

    uploading new files

--- menu.php
+++ menu.php
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<title>Menu</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/w3.css">
-<link href="css/menu.css" rel="stylesheet">
-<body>
-<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%; margin-top: -1%;">
-	<div>
-    <div class="ba col-1">
-      <img src="Images/shuttle.png" class ="ba-pic">
-      <form>
-        <h3>Entrance</h3>
-        <h5>Kids <input type="number" name="kids" id="input" min="0"></h5>  
-        <h5>Adults <input type="number" name="adults" id="input" min="0"></h5>
-        <h5>Discounted <input type="number" name="discounted" id="input" min="0"></h5>
-        <br>
-      </form>
-      <input type="submit" name="submit" id="btn">
-    </div>
-  </div>
-</div>
-<!-- Page Content -->
-<div style="margin-left:25%">
-	<?php include 'pool.html';?>
-  <?php include 'rides.html';?>
-</div>
-</body>
-=======
 <?php include 'header.html' ?>
 <?php
 session_start();
@@ -143,5 +113,4 @@ session_start();
 </div>
 </form>
 </body>
->>>>>>> a4276c5acc621609bb36a51405a3e0b8c3eefe43
 </html>
